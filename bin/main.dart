f5028bdf1e1f96b5b64e0f39c65d6e21058ce251
Johan Jäderblom <jaeder42@gmail.com>
2020-04-06 15:49:50 +0200

    trying to figure out

--- bin/main.dart
+++ bin/main.dart
@@ -5,23 +5,25 @@ import 'package:api/get_data.dart';
 void main(List<String> arguments) async {
   var envVars = Platform.environment;
   var PORT = int.parse(envVars['PORT']);
-  // #docregion bind
   var server = await HttpServer.bind(InternetAddress.loopbackIPv4, PORT);
-  // #enddocregion bind
   print('Listening on localhost:${server.port}');
 
-  // #docregion listen
-  await for (HttpRequest request in server) {
-    var requestedPath = request.requestedUri.toString().split('/')[3];
-    print(requestedPath);
-    if (requestedPath.contains('list')) {
-      var result = await getData();
-      request.response.headers.contentType =
-          ContentType('application', 'json', charset: 'utf-8');
-      request.response.write(result);
-    } else {
-      request.response.write('You\'re lost');
+  try {
+    await for (HttpRequest request in server) {
+      var requestedPath = request.requestedUri.toString().split('/')[3];
+      print(requestedPath);
+      if (requestedPath.contains('list')) {
+        var result = await getData();
+        request.response.headers.contentType =
+            ContentType('application', 'json', charset: 'utf-8');
+        request.response.write(result);
+      } else {
+        request.response.write('You\'re lost');
+      }
+      await request.response.close();
     }
-    await request.response.close();
+    print('WAT');
+  } catch (err) {
+    print(err);
   }
 }
